4fb2a2382d8a991b8e67716d5a1753266c382d36
smeagol <chandramohan.soni@gmail.com>
2017-04-19 01:13:19 +0530

    second commit

--- Linear.lua
+++ Linear.lua
@@ -2,8 +2,20 @@ local Linear = 	torch.class("Linear")
 
 -- init function
 function Linear::__init(inp_neuron,out_neuron)
-	self.output = 
-	self.W = 
-	self.B = 
-	self.gradW = 
-	self.gradB =
+	self.output = nil
+	self.W = torch.tensor(out_neuron,inp_neuron):zero()
+	self.B = torch.tensor(out_neuron,1):zero()
+	self.gradW = torch.tensor(out_neuron,inp_neuron):zero()
+	self.gradB = torch.tensor(out_neuron,1):zero()
+	self.gradInput = nil
+end
+
+-- calculates W* input + B and returns
+function Linear:forward(input)
+	self.output = self.W * input + self.B
+	return self.output
+end
+
+function Linear:backward(input,gradOutput)
+	
+end
