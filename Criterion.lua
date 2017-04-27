1ad1461d63e40caa5127d5c793ec422f25cea442
amitmalav <amitmalav96@gmail.com>
2017-04-27 18:55:17 +0530

    crit

--- Criterion.lua
+++ Criterion.lua
@@ -1,21 +1,28 @@
 local Criterion = torch.class("Criterion")
 
-function Criterion::__init()
+function Criterion:__init()
 	-- body
 end
 
-function Criterion::forward( input, target )
-	-- body
-	for i=1,input:size(1) do
-		max_input = torch.max(input,i)
+function Criterion:forward( input, target )
+	avg_loss = 0
+	for i = 1, input:size(1) do
+		input[i] = input[i]:exp()
+		input[i] = input[i]/(input[i]:sum())
+		tmp = torch.zeros(input:size(1), input:size(2))
+		avg_loss = avg_loss - math.log(input[i][target[i]])
 	end
-	input_exp = input:exp()
-	input_exp = input_exp/input_exp:sum()
-	self.output = -math.log(input[target])
-	return self.output
+	avg_loss = avg_loss/input:size(1)
+	return avg_loss
 end
 
-function Criterion::backward( input, target )
+function Criterion:backward( input, target )
 	-- body
+	gradInput = torch.zeros(input:size(1), input:size(2))
+	for i = 1, input:size(1) do
+    	gradInput[i][target[i]] = 1
+    	gradInput[i] = gradInput[i] - input
+	end
+	return gradInput
 
 end
